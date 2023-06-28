36fb219a6f23176ed8de35161f25d0cb95f323cc
Dumitru Cucu <46863241+dumitruPuggle@users.noreply.github.com>
2023-06-28 08:50:23 +0300

    update config

--- scripts/path_config.py
+++ scripts/path_config.py
@@ -3,6 +3,6 @@ import os
 model_name = "TheBloke/Wizard-Vicuna-7B-Uncensored-HF"
 model_path = os.path.join(os.getcwd(), "model")
 tensorized_path = os.path.join(
-    os.getcwd(), "model", "TheBloke/Wizard-Vicuna-7B-Uncensored-HF.tensors")
+    os.getcwd(), "model", "vicuna-7B-Uncensored-HF.tensors")
 tokenizer_path = os.path.join(
     os.getcwd(), "model")
